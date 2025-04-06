cc4f8b284e3a46ade77ff097dde0ba0180812460
Chiyoshic <877604831@qq.com>
2025-04-06 23:31:50 +0800

    Initialize C++ project with basic structure and main function

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Chiyoshi on 2025/4/6.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
--- includes/b.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by Chiyoshi on 2025/4/6.
-//
