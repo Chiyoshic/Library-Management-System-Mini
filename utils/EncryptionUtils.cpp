8dde7799ae7d3e3ae25238a652b9081c9fb5b5a1
Chiyoshic <877604831@qq.com>
2025-04-07 16:22:53 +0800

    Add EncryptionUtils class and update editor.xml for inspection severities

---
+++ utils/EncryptionUtils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Chiyoshi on 2025/4/7.
+//
+
+#include "EncryptionUtils.h"
---
+++ utils/EncryptionUtils.h
@@ -0,0 +1,16 @@
+//
+// Created by Chiyoshi on 2025/4/7.
+//
+
+#ifndef ENCRYPTIONUTILS_H
+#define ENCRYPTIONUTILS_H
+
+
+
+class EncryptionUtils {
+
+};
+
+
+
+#endif //ENCRYPTIONUTILS_H
